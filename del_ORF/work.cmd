0d2a8fbdc6161da8d774acf734ea93cc12bb9e4e
WZK <cory@hnu.edu.cn>
2018-03-12 20:32:22 +0800

    Update work.cmd

--- del_ORF/work.cmd
+++ del_ORF/work.cmd
@@ -1,1 +1,5 @@
-blastp -query input.fasta -out out.txt -db database -outfmt 6 -num_threads 4 -max_target_seqs 1
+Echo %Date% %Time%
+
+Blastp -query input.fasta -out out.txt -db database -evalue 1e-5 -outfmt 6 -num_threads 4 -max_target_seqs 1
+
+Echo %Date% %Time%
